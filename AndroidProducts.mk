929be322e09f07778c14274929a01a3eccbd9200
archcloudy <cloudguy9@proton.me>
2026-07-12 16:33:45 +0700

    plato: PixelOS BringUp Signed-off-by: archcloudy <cloudguy9@proton.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,4 +4,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_plato.mk
+    $(LOCAL_DIR)/custom_plato.mk
---
+++ custom_plato.mk
@@ -0,0 +1,33 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from plato device
+$(call inherit-product, device/xiaomi/plato/device.mk)
+
+# Inherit some common custom stuff.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
+
+# Screen resolutions
+TARGET_SCREEN_HEIGHT := 2712
+TARGET_SCREEN_WIDTH := 1220
+
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := plato
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_MODEL := 22071212AG
+PRODUCT_NAME := custom_plato
+PRODUCT_SYSTEM_NAME := plato_global
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_CHARACTERISTICS := nosdcard
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildFingerprint=Xiaomi/plato/plato:15/AP3A.240905.015.A2/OS2.0.215.0.VLQMIXM:user/release-keys \
+    DeviceProduct=$(PRODUCT_SYSTEM_NAME)
