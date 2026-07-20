6a3781829092d71161175c39a2a3711cdb23d40b
archcloudy <cloudguy9@proton.me>
2026-07-20 21:25:38 +0700

    plato: Match BuildFingerprint with stock ROM To indicate the firmware is using Global variant By simply adding 'global' so it becomes 'plato_global'
    Signed-off-by: archcloudy <cloudguy9@proton.me>

--- custom_plato.mk
+++ custom_plato.mk
@@ -29,5 +29,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint=Xiaomi/plato/plato:15/AP3A.240905.015.A2/OS2.0.215.0.VLQMIXM:user/release-keys \
+    BuildFingerprint=Xiaomi/plato_global/plato:15/AP3A.240905.015.A2/OS2.0.215.0.VLQMIXM:user/release-keys \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
